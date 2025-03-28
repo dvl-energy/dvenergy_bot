cc3216a5be8f9d84056940ccf8c57e414d3ee250
dvl-energy <d.vladykin.ea@gmail.com>
2025-03-28 11:15:33 +0300

    fix: подключение токена из env

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+import os
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой бот по восстановлению. Введи /stretch или /offday.")
